ecda8f66143cac606f90acfd1c8a4dd9052faae6
Golubovich Dmitry <rikkydn@gmail.com>
2016-04-10 22:56:31 +0300

    Задание №2 (Потренироваться использовать функции, классы, объекты и методы)

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
@@ -1,7 +1,12 @@
 package ru.stqa.ptf.sandbox;
 
 public class MyFirstProgram {
+
     public static void main (String[] args) {
-        System.out.println("Hello, world");
+        Point p1 = new Point(7,11);
+        Point p2 = new Point(7,13);
+        double result = Point.distance(p1,p2);
+
+        System.out.println("Расстояние между точками: " + result);
     }
 }
---
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/Point.java
@@ -0,0 +1,19 @@
+package ru.stqa.ptf.sandbox;
+
+public class Point {
+    public double x;
+    public double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public static double distance (Point p1, Point p2) {
+        double cathetus1 = p1.x - p2.x;
+        double cathetus2 = p1.y - p2.y;
+        
+        double distance = Math.sqrt(Math.pow((cathetus1), 2) + Math.pow((cathetus2), 2));
+        return distance;
+    }
+}
