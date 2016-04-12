15ead6fc846ba706b1a60581a4abedec3910fae0
riikki <riikkimarley@gmail.com>
2016-04-12 08:48:18 -0700

    add examples from the video

---
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/Rectangle.java
@@ -0,0 +1,15 @@
+package ru.stqa.ptf.sandbox;
+
+/**
+ * Created by user on 4/12/2016.
+ */
+public class Rectangle {
+
+	public double a;
+	public double b;
+
+	public Rectangle(double a, double b) {
+		this.a=a;
+		this.b=b;
+	}
+}
---
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/Square.java
@@ -0,0 +1,13 @@
+package ru.stqa.ptf.sandbox;
+
+/**
+ * Created by user on 4/12/2016.
+ */
+public class Square {
+
+	public double l;
+
+	public Square(double len){
+		this.l=len;
+	}
+}
